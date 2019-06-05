8d41b3b852a800f9deb180aa8029f9f5d97fa669
Ekaterina Lapkovskaja <ekaterina.lapkovskaja@waveaccess.ru>
2019-06-05 14:08:17 +0300

    remove pods

---
+++ PullUpController/PullUpController.h
@@ -0,0 +1,19 @@
+//
+//  PullUpController.h
+//  PullUpController
+//
+//  Created by user on 05/06/2019.
+//  Copyright © 2019 WaveAccess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PullUpController.
+FOUNDATION_EXPORT double PullUpControllerVersionNumber;
+
+//! Project version string for PullUpController.
+FOUNDATION_EXPORT const unsigned char PullUpControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PullUpController/PublicHeader.h>
+
+
